1a362b81ab5176eadb430a5a881e80bacbfbf83b
hasanraz <hasanraz2009@gmail.com>
2021-05-10 21:17:31 +0530

    Updated commit

--- A4-Multipage Website/js/carousel.js
+++ A4-Multipage Website/js/carousel.js
@@ -1,8 +1,9 @@
-import "../css/style.css";
 
-let liEls = document.querySelectorAll("carousel_img carousel_img_list");
+
+let liEls = document.getElementsByClassName("carousel_img_list");
 let index = 0;
 window.show = function (increase) {
+  console.log("Inside the show");
   index = index + increase;
   index = Math.min(Math.max(index, 0), liEls.length - 1);
   liEls[index].scrollIntoView({ behavior: "smooth" });

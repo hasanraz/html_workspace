3c90544b21771236b6c1d1273b1821c8d141ac75
shresthashashank <shashank1611.sp@gmail.com>
2021-05-10 03:36:13 -0500

    Coffe Shop 1.0 (Carousel functionality debugging needed)

---
+++ Assignment 4/js/app.js
@@ -0,0 +1,51 @@
+//Variable used for the current position of the slider. Initial value is 0.
+var current_pos = 0;
+
+//Assigning left and right arrows respectively with queryselector
+var slider_img = document.querySelectorAll(".images");
+var arrowLeft = document.getElementsByClassName(".prev");
+var arrowRight = document.getElementsByClassName(".next");
+//Function to clear out all the images.
+function clear() {
+  for (let i = 0; i < slider_img.length; i++) {
+    slider_img[i].style.display = "none";
+  }
+}
+
+//Starts the slider
+function carousel_start() {
+  clear();
+  slider_img[0].style.display = "block";
+}
+
+function left_slider() {
+  clear();
+  slider_img[current_pos - 1].style.display = "block";
+  current_pos--;
+}
+
+function right_slider() {
+  clear();
+  slider_img[current_pos + 1].style.display = "block";
+  current_pos++;
+}
+
+//Creating an event listener for left arrow
+arrowLeft.addEventListener("click", function () {
+  if (current_pos === 0) {
+    current_pos = slider_img.length;
+  }
+  left_slider();
+});
+s;
+
+//Creating an event listener for right arrow
+
+arrowRight.addEventListener("click", function () {
+  if (current_pos === slider_img.length - 1) {
+    current_pos = 0;
+  }
+  right_slider();
+});
+
+//carousel_start();
